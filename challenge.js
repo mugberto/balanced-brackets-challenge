b1e85b2d41476b7dd41de1ee1404a12e38c90335
Hubert MUGABO <mugberto@gmail.com>
2021-07-18 01:37:32 +0200

    solution 1

--- challenge.js
+++ challenge.js
@@ -1,5 +1,18 @@
 function balancedBrackets(string) {
   // your code here
+  const pairBrackets = { '[': ']', '(': ')', '{': '}'};
+  const strArray = string.split('');
+  let stack = [];
+
+  strArray.forEach( char => {
+    if (char === '[' || char === '{' || char === '(') {
+      stack.push(char);
+    }else if ( char === pairBrackets[stack[stack.length-1]]) {
+      stack.pop();
+    }    
+  });
+
+  return stack.length === 0;
 }
 
 console.log(balancedBrackets('(hello)[world]'))
